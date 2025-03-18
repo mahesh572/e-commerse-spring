a68ec4ef873998ea9b23c8797f2739e469d29d78
mahesh572 <maheshgaade572@gmail.com>
2025-03-18 12:53:15 +0530

    first user-mgmt-app commit
    first user-mgmt-app commit

---
+++ user-mgmt/src/test/java/com/mayuktha/usersmgmt/UserMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayuktha.usersmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
